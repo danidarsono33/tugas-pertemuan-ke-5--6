87ce8a6cd85748a679ce55ea80daa52ff736632b
danidarsono33 <danidarsono33@gmail.com>
2020-11-04 18:16:52 +0700

    latihan-python

---
+++ 01.latihan1.py
@@ -0,0 +1,35 @@
+#latihan pyton pertemuan ke 5
+
+nama= input (' nama lengkap anda :')
+print(nama)
+
+panggilan= input ('nama panggilan anda :')
+print(panggilan)
+
+npm = input ('nomor npm anda ')
+print(npm)
+
+umur =input ('umur anda :')
+print(umur)
+
+tempat = input ('tempat lahir anda :')
+print (tempat)
+
+tanggal = input(' tanggal lahir anda :')
+print(tanggal)
+
+telpon=input ('nomor telpon anda :')
+
+alamat = input ('alamat anda :')
+
+########################################################################################################
+
+print ('assalamualaikum')
+print ('let me  introduce my self .my name is '+str (nama) ,'but you can call me '+str(panggilan))
+print ('MY NPM is ' +str(npm),'I was born in'+str (tempat),'and i am '+str(umur),'years old. i am very glad if you want to invit')
+print('my house in '+str(alamat),'so,dont forget to call me before with the number ',str (telpon),'')
+
+
+
+
+
---
+++ 02.lab2.py
@@ -0,0 +1,14 @@
+# pertemuan ke 6 lab 2
+
+a =int(input ("masukan nilai a: "))
+b =int(input ('masukan nilai b :'))
+print ('variabel a =",a)')
+print ('variabel b =",b)')
+print ('hasil penggabungan {1}&{0}=%d'.format (a,b)%(a+b))
+
+
+#konversi nilai variabel
+a=int(a)
+b=int(b)
+print ("hasil penjumlahan {1}+{0}=%d".format (a,b)%(a+b))
+print ("hasil pembagian  {1}/{0}=%d".format (a,b)%(a/b))
